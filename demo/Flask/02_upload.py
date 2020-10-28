fcd11443993c2234465c8b9d69cecef236d5f211
NmaicaiBJZ <68455146+NmaicaiBJZ@users.noreply.github.com>
2020-10-28 15:58:26 +0800

    Add files via upload

--- demo/Flask/02_upload.py
+++ demo/Flask/02_upload.py
@@ -5,24 +5,24 @@ app = Flask(__name__)
 #127.0.0.1:5000/upload
 @app.route("/upload", methods={"GET","POST"})
 def upload():
-	"""表示前端发送过来的数据"""
-	file_obj = request.files.get("pic")
-	if file_obj is None: #表示没有发送过来文件
-		return "未上传文件"
+    """表示前端发送过来的数据"""
+    file_obj = request.files.get("pic")
+    if file_obj is None: #表示没有发送过来文件
+        return "未上传文件"
 
-	# #将文件保存到本地
-	# #1.创建一个文件
-	# f = open("./demo.png","wb")
-	# #2.向文件写内容
-	# data = file_obj.read()
-	# f.write(data)
-	# #3.关闭文件
-	# f.close()
-	# return "上传成功"
+    # #将文件保存到本地
+    # #1.创建一个文件
+    # f = open("./demo.png","wb")
+    # #2.向文件写内容
+    # data = file_obj.read()
+    # f.write(data)
+    # #3.关闭文件
+    # f.close()
+    # return "上传成功"
 
-	#直接使用上传的文件对象
-	file_obj.save("./demo1.png")
-	return "上传成功"
+    #直接使用上传的文件对象
+    file_obj.save("./demo1.png")
+    return "上传成功"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- demo/Flask/03_login.py
+++ demo/Flask/03_login.py
@@ -4,20 +4,20 @@ app = Flask(__name__)
 
 @app.route("/login", methods={"GET","POST"})
 def login():
-	#name = request.form.get()
-	#pwd = requset.form.get()
-	name = ""
-	pwd = ""
-	if name != "zhangsan" or pwd != "admin":
-		#使用abort函数可以立即终止视图函数的执行
-		#并可以返回前端特定的信息
-		#1、传递状态信息，必须是标准的http状态码:400.404.500.
-		abort(400)
-		#2、传递响应信息
-		resp = Response("login faild ")
-		abort(resp)
+    #name = request.form.get()
+    #pwd = requset.form.get()
+    name = ""
+    pwd = ""
+    if name != "zhangsan" or pwd != "admin":
+        #使用abort函数可以立即终止视图函数的执行
+        #并可以返回前端特定的信息
+        #1、传递状态信息，必须是标准的http状态码:400.404.500.
+        abort(400)
+        #2、传递响应信息
+        resp = Response("login faild ")
+        abort(resp)
 
-	return "login success!"
+    return "login success!"
 
 
 
